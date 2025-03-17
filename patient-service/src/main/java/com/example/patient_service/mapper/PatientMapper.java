7cf51dffc894e915de13fba21fd7e64c6cfab2d8
Kai <dk786161616@gmail.com>
2025-03-17 14:24:11 +0530

    added patient service and dto

---
+++ patient-service/src/main/java/com/example/patient_service/mapper/PatientMapper.java
@@ -0,0 +1,17 @@
+package com.example.patient_service.mapper;
+
+import com.example.patient_service.dto.PatientResponseDTO;
+import com.example.patient_service.model.Patient;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient){
+        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
+        patientResponseDTO.setId(patient.getId().toString());
+        patientResponseDTO.setName(patient.getName());
+        patientResponseDTO.setAddress(patient.getAddress());
+        patientResponseDTO.setEmail(patient.getEmail());
+        patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientResponseDTO;
+    }
+}
