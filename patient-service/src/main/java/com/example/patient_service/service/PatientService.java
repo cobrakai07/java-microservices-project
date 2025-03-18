1a11f690507650a41d4249ee14b36c8bebb89c25
Kai <dk786161616@gmail.com>
2025-03-18 13:58:46 +0530

    added validators and completed CRUD

--- patient-service/src/main/java/com/example/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/example/patient_service/service/PatientService.java
@@ -3,12 +3,16 @@ package com.example.patient_service.service;
 import com.example.patient_service.dto.PatientRequestDTO;
 import com.example.patient_service.dto.PatientResponseDTO;
 import com.example.patient_service.exception.EmailAlreadyExistsException;
+import com.example.patient_service.exception.PatientNotFoundException;
 import com.example.patient_service.mapper.PatientMapper;
 import com.example.patient_service.model.Patient;
 import com.example.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -31,5 +35,23 @@ public class PatientService {
         }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
+    }
+
+    public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
+        Patient patient = patientRepository.findById(id).orElseThrow(()->new PatientNotFoundException("Patient not found with ID: "+ id));
+        if(patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(), id)){
+            throw new EmailAlreadyExistsException("A patient with this email already exists"+ patientRequestDTO.getEmail());
+        }
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+        Patient updatedPatient = patientRepository.save(patient);
+        return PatientMapper.toDTO(updatedPatient);
+    }
+
+    public void deletePatient(UUID id){
+        patientRepository.deleteById(id);
     }
 }
